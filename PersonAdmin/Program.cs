d08113901f01c2e44a65063ecae5cc6df51f184b
Hesham Ouda <hesham.ouda@students.fhnw.ch>
2022-02-17 13:23:53 +0100

    First .Net App

--- PersonAdmin/Program.cs
+++ PersonAdmin/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Reflection;
+using PersonAdminLib;
+
+namespace PersonAdmin
+{
+
+    class Program
+    {
+       static void Main(String[] args)
+        {
+            Console.WriteLine($"My first C# Program:{ Assembly.GetExecutingAssembly().GetName().Version})");
+
+            var person = new Person("Hesham", "Ouda");
+
+            Console.WriteLine($"Name: {person.Firstname} {person.Surname}");
+
+            Console.WriteLine("Press any key to quit");
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ PersonAdminLib/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+
+
+
+namespace PersonAdminLib
+{
+    public class Person
+    {
+
+       
+
+        public Person(string firstname, string surname)
+        {
+            Firstname = firstname;
+            Surname = surname;
+        }
+        public string Firstname { get; }
+        public string Surname { get; }
+
+    }
+    
+}
